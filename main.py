97cf1e37c633f0632f4fc6646078da5a0ceb8212
Muxite <muk.chunpongtong@gmail.com>
2023-12-27 21:57:41 -0700

    completed most of algorithm code
    Now just need to create an easy way to make maps and targets, as well as visualize the results.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ dimensions = 2
 nodes = []  # this is the map. Each node is a class, open and closed hold pointers.
 nodes_open = []
 nodes_closed = []
-neighbours = []  # describes the neighbour locations
+neighbours = []  # describes the neighbour locations and distance as the last one
 start = None  # node to start at
 target = None  # node to pathfind towards
 
@@ -47,10 +47,12 @@ standard_neighbours(dimensions)
 
 # node class stores data
 class Node:
-    def __init__(self, location, f_cost, state):
-        self.location = location
-        self.f_cost = f_cost  # distance to target + distance to start
+    def __init__(self, location, state, g_cost, h_cost):
+        self.location = location  # x, y, maybe z
+        self.g_cost = g_cost  # distance to start
+        self.h_cost = h_cost  # distance to target
         self.state = state  # 0 base, 1 open, 2 closed
+        self.parent = None  # will set this later
 
 
 def a_star():
@@ -66,18 +68,49 @@ def a_star():
         lowest = 99999999
         index = -1
         for j in range(len(nodes_list)):
-            if nodes_list[j].f_cost < lowest:
+            if nodes_list[j].g_cost + nodes_list[j].h_cost < lowest:
                 index = j
         return index
 
     # main loop
     for i in range(0, 10000):
-        current = nodes_open[f_min_i(nodes_open)]  # current = lowest f_cost that is open
+        # except for the first loop
+        if i == 0:
+            if dimensions == 2:
+                current = nodes[start[0]][start[1]]
+            elif dimensions == 3:
+                current = nodes[start[0]][start[1]][start[2]]
+        else:
+            current = nodes_open[f_min_i(nodes_open)]  # current = lowest f_cost that is open
         nodes_open.remove(current)  # these are just pointers
         nodes_closed.append(current)
 
         # stop if at the target
-        if current.location == target.location:
+        if current.location == target:
             print("Pathfinding Finished")
             return
 
+        # for neighbours of current
+        for j in range(len(neighbours)):
+            neighbour = current  # this will change
+            try:
+                n_location = current.location + neighbours[j]
+                if dimensions == 2:
+                    neighbour = nodes[n_location[0]][n_location[1]]
+                elif dimensions == 3:
+                    neighbour = nodes[n_location[0]][n_location[1]][n_location[2]]
+            except IndexError:
+                j += 1  # skip to next neighbour
+            if neighbour is not current:
+                # if not closed
+                if neighbour.state != 2:
+                    # add the previous g_cost with the distance from it to this neighbour cell
+                    path_length = current.g_cost + neighbours[i][dimensions]
+                    if path_length < neighbour.g_cost or neighbour.state != 1:
+                        # update f_cost
+                        neighbour.g_cost = path_length
+                        neighbour.h_cost = distance(neighbour.location, target)
+                        # set the parent to retrace steps later
+                        neighbour.parent = current
+                        if neighbour not in nodes_open:
+                            nodes_open.append(neighbour)
